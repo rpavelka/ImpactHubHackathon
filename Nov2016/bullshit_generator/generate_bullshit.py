5413f966b0f2d2889d97eb85231c4a3dd905617c
Roman Pavelka <rpavelka@redhat.com>
2016-11-16 20:34:22 +0100

    Init hackathon repo

---
+++ Nov2016/bullshit_generator/generate_bullshit.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+"""This file just generates bullshit"""
+
+
+def build_probabilities(korpus_fn: str):
+    return [], dict()
+
+
+def generate_bullshit(words: list, probabilities: dict):
+    return ""
+
+
+if __name__ == "__main__":
+    words, probabilities = build_probabilities("sample_korpus_text_1.txt")
+    print(generate_bullshit(words, probabilities))
