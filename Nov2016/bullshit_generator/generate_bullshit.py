b7db55a524f8ce2222eb206ebe319813894f9fd2
Roman Pavelka <rpavelka@redhat.com>
2016-11-16 20:49:05 +0100

    Propose design of the bullshit

--- Nov2016/bullshit_generator/generate_bullshit.py
+++ Nov2016/bullshit_generator/generate_bullshit.py
@@ -2,8 +2,19 @@
 """This file just generates bullshit"""
 
 
-def build_probabilities(korpus_fn: str):
-    return [], dict()
+def filter_out_sentences(raw_korpus: str):
+    return ""
+
+
+def build_probabilities(korpus_fn: str, len_of_sequence):
+    """
+    Takes a filename, reads the content and prepares list of words to use
+    and probabilities of wrod sequences
+    """
+    with open(korpus_fn) as f:
+        raw_korpus = f.read()
+    korpus = filter_out_sentences(raw_korpus)
+    return korpus.split(" "), dict()
 
 
 def generate_bullshit(words: list, probabilities: dict):
@@ -11,5 +22,5 @@ def generate_bullshit(words: list, probabilities: dict):
 
 
 if __name__ == "__main__":
-    words, probabilities = build_probabilities("sample_korpus_text_1.txt")
+    words, probabilities = build_probabilities("sample_korpus_text_1.txt", 2)
     print(generate_bullshit(words, probabilities))
